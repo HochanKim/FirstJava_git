51b0899ff816af3404c65fdaafcbf62e56d99d74
tj-bu-708-22PC <tj-bu-708-22PC@DESKTOP-GSK5EAU>
2024-06-19 15:29:43 +0900

    패키지 special의 Array01.java 주석 추가

--- Java_Study/src/special/Array01.java
+++ Java_Study/src/special/Array01.java
@@ -5,7 +5,7 @@ public class Array01 {
 
 	public static void main(String[] args) {
 		// 배열
-		int arr[] = {3, 5, 2, 4, 1};
+		int arr[] = {3, 5, 2, 4, 1};	// 배열 리스트
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
 		System.out.println();
