00882789c318dcc0539443d260e54e5553990cbe
ghcks <ghcks@mrlumpen>
2024-07-08 21:25:41 +0900

    2024.07.08 (21:25)

--- Java_Study/src/special/FruitStore.java
+++ Java_Study/src/special/FruitStore.java
@@ -17,14 +17,15 @@ public class FruitStore {
 			System.out.print("메뉴 선택 : ");
 			int menu = scan.nextInt();
 			
-			if(menu == 1) {
+			if(menu == 1) {	// 조건문 1
 				System.out.print("과일명 : ");
 				String fruit = scan.next();
-				boolean flag = false;
+				boolean flag = false;	// 중복 체크를 위한 불형 변수선언
 				
+				// 특정 조건 반복문 실행
 				for(HashMap<String, Object> item : list) {
-					if(item.get("fruit").equals(fruit)) {
-						flag = true;
+					if(item.get("fruit").equals(fruit)) {	// 과일명 "fruit"의 중복일 경우 조건
+						flag = true;	// 중복 체크를 위한 불형 변수 
 						System.out.print("가격 입력 : ");
 						int price = scan.nextInt();	// 가격 입력한 값 저장하는 변수
 					
@@ -40,7 +41,7 @@ public class FruitStore {
 					}
 				}
 				
-				if(flag == false) {
+				if(flag == false) {	// 불형 변수에 의한 조건
 					HashMap<String, Object> map = new HashMap<>();
 					System.out.print("가격 입력 : ");
 					int price = scan.nextInt();	// 가격 입력한 값 저장하는 변수
@@ -59,12 +60,52 @@ public class FruitStore {
 			}
 			System.out.println(list);
 			
-			if(menu == 2) {
+			if(menu == 2) { // 조건문 2
+				System.out.print("과일명 : ");
+				String fruit = scan.next();
 				
+				// 특정 조건 반복문 실행
+				while (true) {
+					HashMap<String, Object> map = new HashMap<>();
+					for(int i=0; i<list.size(); i++) {
+						int num = (int) list.get(i).get("num");
+						
+						System.out.print("판매 개수 입력 : ");
+						int inputNum = scan.nextInt();
+						if(inputNum < 0) {	
+							System.out.println("잘못된 입력입니다. (최소 0까지 입력가능)");
+							continue;
+						}	
+						
+						if(fruit.equals((String) list.get(i).get("fruit"))) {
+							list.get(i).put("num", (num - inputNum));
+								
+							// 잘못된 개수 계산시 조건문
+							if(num-inputNum < 0) {
+								System.out.println("개수가 부족합니다.");
+								list.get(i).put("num", (num - inputNum + inputNum));
+								continue;
+							}
+						} else {
+							System.out.println("판매되었습니다.");
+							System.out.println(fruit+ "의 현재 남은 개수는 "+ (num - inputNum) +"개 입니다.");
+						}
+						break;
+					}
+					break;
+				}
 			}
 			
-			if(menu == 3) {
-				
+			if(menu == 3) {	// 조건문3
+				System.out.print("과일명 : ");
+				String fruit = scan.next();
+				for(int i=0; i<list.size(); i++) {
+					fruit = (String) list.get(i).get("fruit");
+					if(fruit.equals(fruit)) {
+						int num = (int) list.get(i).get("num");
+						System.out.println(fruit+ "의 현재 남은 개수는 "+ num +"개 입니다.");
+					}
+				}
 			}
 			
 			if(menu < 1 || menu > 3) {
---
+++ Java_Study/src/special/ManyArray_review.java
@@ -0,0 +1,14 @@
+package special;
+//1. (0.0) ~ (2.2) 까지는 1부터 10까지 랜덤한 숫자
+//2. 각 행의 마지막 열은 해당 행의 총 합
+//3. 마지막행 각 열은 앞선 동일 위치 열들의 총 합
+//4. (3,3)은 전체 총 합
+
+public class ManyArray_review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
